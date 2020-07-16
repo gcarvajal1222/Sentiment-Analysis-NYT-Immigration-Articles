7c4e466180b855c6e653199c5881e67ab1bb77a2
gcarvajal1222 <36053537+gcarvajal1222@users.noreply.github.com>
2020-07-16 18:32:23 -0400

    built sentiment app

---
+++ C# ML predictions/ML C# code/SentimentRazor/SentimentRazor/Pages/Index.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.ML;
+using SentimentRazorML.Model;
+
+namespace SentimentRazor.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        private readonly PredictionEnginePool<ModelInput, ModelOutput> _predictionEnginePool;
+
+        public IndexModel(PredictionEnginePool<ModelInput, ModelOutput> predictionEnginePool)
+        {
+            _predictionEnginePool = predictionEnginePool;
+        }
+
+        public void OnGet()
+        {
+
+        }
+
+        public IActionResult OnGetAnalyzeSentiment([FromQuery] string text)
+        {
+            if (String.IsNullOrEmpty(text)) return Content("neutral");
+            var input = new ModelInput { CleanText = text };
+            var prediction = _predictionEnginePool.Predict(input);
+            var sentiment = prediction.Prediction;
+            return Content(sentiment);
+        }
+    }
+}
