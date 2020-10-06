52b4039bbec1514a4f38d80b50d8130c0a029798
gcarvajal1222 <gabriel.carvajal@valpo.edu>
2020-10-06 12:27:58 -0400

    right fields in the database

---
+++ Web App Razor Predictions/ML C# code/SentimentRazor/SentimentRazor/Migrations/20201006162643_buildnewdatabase.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SentimentRazor.Migrations
+{
+    public partial class buildnewdatabase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "SavePrediction",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Text = table.Column<string>(nullable: false),
+                    Prediction = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SavePrediction", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "SavePrediction");
+        }
+    }
+}
