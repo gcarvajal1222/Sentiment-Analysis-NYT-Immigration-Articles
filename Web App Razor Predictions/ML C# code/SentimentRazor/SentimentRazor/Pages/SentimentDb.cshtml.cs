9310e50289364397cb39b1be70ec7954b95f4f89
Gabriel Carvajal <gabrielcarvajal@chevron.com>
2021-01-10 23:39:13 -0500

    added actual sentiment in predictions

--- Web App Razor Predictions/ML C# code/SentimentRazor/SentimentRazor/Pages/SentimentDb.cshtml.cs
+++ Web App Razor Predictions/ML C# code/SentimentRazor/SentimentRazor/Pages/SentimentDb.cshtml.cs
@@ -20,23 +20,50 @@ namespace SentimentRazor.Pages
 
         public IEnumerable<SentimentResult> SentimentResults { get; set; }
 
-        //public async Task OnGet()
-        //{
-        //    SentimentResults = await _db.SavePrediction.ToListAsync();
-        //}
+        public async Task OnGet()
+        {
+            SentimentResults = await _db.SavePrediction.ToListAsync();
+        }
 
         public async Task<IActionResult> OnPostDelete(int id)
         {
-            var book = await _db.SavePrediction.FindAsync(id);
-            if (book == null)
+            var prediction = await _db.SavePrediction.FindAsync(id);
+            if (prediction == null)
             {
                 return NotFound();
             }
-            _db.SavePrediction.Remove(book);
+            _db.SavePrediction.Remove(prediction);
             await _db.SaveChangesAsync();
 
             return RedirectToPage("SentimentDb");
         }
 
+        //public async Task<IActionResult> OnGetEdit(int? id)
+        //{
+        //    if (id==null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    var prediction = await _db.SavePrediction.FindAsync(id);
+        //    if (prediction==null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    return ViewComponent(prediction.ToString());
+        //}
+
+
+        //public async Task<IActionResult> OnPosEdit(int id)
+        //{
+        //    var prediction = await _db.SavePrediction.FindAsync(id);
+        //    if (prediction == null)
+        //    {
+        //        return NotFound();
+        //    }
+        //    _db.SavePrediction.Update(prediction);
+        //    await _db.SaveChangesAsync();
+
+        //    return RedirectToPage("SentimentDb");
+        //}
     }
 }
